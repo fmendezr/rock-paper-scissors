d379eccf4866dde2cbd3ccafc1638787533e62d3
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-04 20:53:15 -0400

    Create play round function

--- app.js
+++ app.js
@@ -2,7 +2,31 @@ const options = ['rock', 'paper', 'scissors'];
 
 const getComputerChoice = () => {
     return options[Math.floor(Math.random()*3)]
-}
+};
 
+const playRound = (playerSelection, computerSelection=getComputerChoice()) => {
+    if (playerSelection.toLowerCase === computerSelection){
+        return "Tie! You and Computer Chose the Same Thing"
+    }
+    switch(playerSelection.toLowerCase()){
+        case "rock":
+            if (computerSelection === 'paper'){
+                return "You Lose! Paper beats Rock"
+            } else {
+                return "You Win! Paper beats Scissors"
+            };
+        case "paper":
+            if (computerSelection === "scissors"){
+                return "You Lose! Scissors beats Paper"
+            } else {
+                return "You Win! Paper beats Rock"
+            };
+        case "scissors":
+            if (computerSelection === "rock"){
+                return "You Loose! Rock beats Scissors"
+            } else {
+                return "You Win! Scissors beats Paper"
+            };
+    }; 
+};
 
-console.log(getComputerChoice())
